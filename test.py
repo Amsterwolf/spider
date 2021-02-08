8a64af3feae6ace0b5d5c86b1a78a01b50255de0
Amsterwolf <2497695115@qq.com>
2021-02-09 00:19:48 +0800

    2/9

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
-from selenium import webdriver
+import re
+import requests
+from bs4 import BeautifulSoup
+from lxml import etree
+url="http://www.santostang.com"
+headers={
+    'user_agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
 
-options = webdriver.ChromeOptions()
-# 禁用图片，CSS，JS
-prefs = {
-    'profile.default_content_setting_values': {
-        'images': 2,
-        'permissions.default.stylesheet': 2,
-        'javascript': 2
-    }
 }
-options.add_experimental_option('prefs', prefs)
-# 下载的geckodriver的存储位置
-driver=webdriver.Chrome(executable_path=r'C:\Users\林\Desktop\chromedriver.exe',options=options)
-# 自动访问的网站
-driver.get("http://www.santostang.com/2018/07/04/hello-world/")
+r=requests.get(url,headers=headers)
+
+html=etree.HTML(r.text)
+title_list=html.xpath('/html/body/div//article/header/h1/a/text()')
+print(title_list)
+
+
+
